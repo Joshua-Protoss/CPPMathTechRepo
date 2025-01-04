8e43acf5d5c85f151e363dddfc6704fda7880f68
Muhammad Joshua <72489749+Joshua-Protoss@users.noreply.github.com>
2025-01-04 08:18:15 +0700

    Update gradientDC.h

--- gradientDC.h
+++ gradientDC.h
@@ -14,6 +14,22 @@ class gradientDC
         // Function to add a pointer to the object function
         void SetObjectFcn(std::function<double(std::vector<double>*)> objectFcn);
 
+        // Function to set the start point (initial) -> set the number of degree of freedom.
+        void SetStartPoint(const std::vector<double> startPoint);
+
+        // Function to set the step size
+        void SetStepSize(double stepSize);
+
+        // Function to set the maximum number of iterations
+        void SetMaxIterations(int maxIterations);
+
+        // Function to set the gradient magnitude threshold (stopping condition)
+        void SetGradientThresh(double gradientThresh);
+
+        // Funcion to perform the optimization
+        bool Optimize(std::vector<double> *funcloc, double *funcVal);
+
+
 };
 
 #endif
