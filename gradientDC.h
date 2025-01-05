e9d427b1c85c69459cbae278802e8ecaa02dcb66
Muhammad Joshua <72489749+Joshua-Protoss@users.noreply.github.com>
2025-01-05 19:21:20 +0700

    Update gradientDC.h

--- gradientDC.h
+++ gradientDC.h
@@ -29,7 +29,41 @@ class gradientDC
         // Funcion to perform the optimization
         bool Optimize(std::vector<double> *funcloc, double *funcVal);
 
+    private:
+     // Function to compute the gradient in the specified dimension
+     double ComputedGradient(int dim);
 
+     // Function to compute the gradient vector
+     std::vector<double> ComputeGradientVector();
+
+     // Function to compute the gradient magnitude
+     double ComputeGradientMagnitude(std::vector<double> gradientVector);
+    
+    // Private variables
+    private:
+        // The number of degrees of freedom of the system
+        int m_nDims;
+
+        // The step size
+        double m_stepSize;
+
+        // The maximum number of iterations
+        int m_maxIter;
+
+        // The gradient step size (h)
+        double m_h;
+
+        // The gradient magnitude threshold (stopping condition)
+        double m_gradientThresh;
+
+        // The start point
+        std::vector<double> m_startPoint;
+
+        // The current point
+        std::vector<double> m_currentPoint;
+
+        // Function-pointer to the object function
+        std::function<double(std::vector<double>*)> m_objectFcn;
 };
 
 #endif
